c128292f79e31affca8dc9e0ab16748ce47c6bb6
DerianDev <derian.nazareno@hotmail.com>
2022-10-19 15:14:25 -0500

    aprendiendo Async y await

---
+++ js-async-await/add.js
@@ -0,0 +1,8 @@
+export function add (x,y){
+    return x + y;
+}
+export function multiply (x,y){
+    return x * y;
+}
+export const title = 'modulos Ecma';
+export default title
---
+++ js-async-await/index.js
@@ -0,0 +1,29 @@
+const ul = document.createElement("ul");
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     data.forEach(function (post) {
+//       const li = document.createElement("li");
+//       li.innerText = post.title;
+//       ul.append(li);
+//     });
+//     document.body.append(ul); 
+//   });
+
+async function loadData(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    data.forEach(function (post) {
+        const li = document.createElement("li");
+        li.innerText = post.title;
+        ul.append(li);
+      });
+      document.body.append(ul);
+}
+
+loadData()
+console.log("linea 1");
