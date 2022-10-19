ea108eafa906291f10746c84af905f38434776f7
DerianDev <derian.nazareno@hotmail.com>
2022-10-19 13:59:36 -0500

    aprendiendo metodos arrays

--- js-string-literals/index.js
+++ js-string-literals/index.js
@@ -5,7 +5,7 @@ const isAuthorized = true;
 
 const button = document.createElement("button");
 button.innerText = "click me"
-button.style = `background: ${isAuthorized ? 'blue' : 'red'}; color: ${color};`
+button.style = `background: ${isAuthorized ? 'blue' : 'red'}; color: ${color};` //se realizo un string literals para poder concatenar los estilos 
 
 
 button.addEventListener("click", () => {
